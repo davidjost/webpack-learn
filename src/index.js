2dfa374f33a80e8b988f1020ea890146c8377be6
David Jost <david.jost@rewe-digital.com>
2019-06-06 12:19:44 +0200

    did some more of the tut

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import { groupBy } from "lodash-es"
+import people from "./people.js"
+
+import './style.scss'
+import './image-example.js'
+
+const managerGroups = groupBy(people, "manager")
+
 const root = document.createElement("div")
-root.innerHTML = `<p>Hello Webpack.</p>`
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
 document.body.appendChild(root)
